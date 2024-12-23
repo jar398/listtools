7ccd924388885278271d58a8a02c015a48de9b58
Jonathan A. Rees <jar398@mumble.net>
2024-12-22 21:28:12 -0500

    fix typos in ranks.py

---
+++ src/ranks.py
@@ -0,0 +1,52 @@
+# Borrowed from Open Tree of Life, with no attribution since this is
+# not 'creative expression'
+
+ranks = ["domain",
+         "superkingdom",
+         "kingdom",
+         "subkingdom",
+         "division",            # h2007
+         "infrakingdom",		# worms
+         "superphylum",
+         "phylum",
+         "subphylum",
+         "infraphylum",			# worms
+         "subdivision",			# worms
+         "superclass",
+         "class",
+         "subclass",
+         "infraclass",
+         "subterclass",         # worms Colobognatha
+         "cohort",              # NCBI Polyneoptera
+         "subcohort",           # NCBI
+         "superorder",
+         "order",
+         "suborder",
+         "infraorder",
+         "parvorder",
+         "section",				# worms - note, there are two 'section' ranks
+         "subsection",			# worms
+         "superfamily",
+         "family",
+         "subfamily",
+         "supertribe",			# worms
+         "tribe",
+         "subtribe",
+         "genus",
+         "subgenus",
+         "species group",
+         "species subgroup",
+         "species",
+         "infraspecificname",
+         "subspecies",
+         "natio",               # worms
+         "variety",
+         "varietas",
+         "subvariety",
+         "form",                # 2016 GBIF
+         "forma",
+         "subform",
+         "cluster",
+         ]
+
+ranks_dict = {name: i for (name, i) in zip(ranks[::-1], range(0, len(ranks)))}
