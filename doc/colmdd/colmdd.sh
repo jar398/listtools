5aafc84fbdc91ba85644d363ac95104a194e100e
Jonathan A. Rees <jar398@mumble.net>
2025-12-02 17:39:21 -0500

    Elaborate example script colmdd.sh

--- doc/colmdd/colmdd.sh
+++ doc/colmdd/colmdd.sh
@@ -1,19 +1,20 @@
-# Might require bash and/or gnu make
+# Sample run of the alignment tool.
 
+# Perform manual steps.  Then run this file as an ordinary shell
+# script using bash or any other Unix shell.
+
+# MANUAL STEP: Get listtools.
+# Go to https://github.com/jar398/listtools.
+# Clone the repo (under 'Code' / 'Clone' / HTTP or the protocol of your choice).
+# Edit L below to point to your local clone of listtools.
+
+L=~/g/listtools
+P=$L/src
+
+# MANUAL STEP: Get COL 2024.
 # Go to checklistbank.org.  Log in.  Go to 'Downloads'.  Find COL24.
 # Download with these parameters: DwCA output, Mammalia root taxon.
 # Save .zip file to $L/in/.
-
-# Go to zenodo.org.  Search for "Mammal diversity".  Select "Mammal
-# diversity database".  Select "Version v2.0".  Under "Files" select
-# "MDD_v2.0_6759species.csv".  Click "Download".  
-# Find MDD_v2.0_6759species.csv on your computer (e.g. in 'Downloads'). 
-# Move it to $L/in/.
-# Run conversion program MDD-DwC-mapping/ on it.
-
-L=~/g/listtools
-M=~/g/MDD-DwC-mapping
-P=$L/src
 
 # Prepare CoL for alignment
 unzip -u $L/in/2b1541bc-12e7-4200-833b-7ae02e1d5f35.zip -d col24-mammals
@@ -22,7 +23,22 @@ $P/extract_names.py < col24-mammals-clean.csv \
 	| gnparser -s \
 	| $P/use_gnparse.py --source col24-mammals-clean.csv > col24-mammals.csv
 
-# Prepare MDD for alignment
+# MANUAL STEP: Get tool for converting MDD files to DwC.
+# Go to https://github.com/jar398/MDD-DwC-mapping.
+# Clone the repo (under 'Code' / 'Clone' / HTTP or the protocol of your choice).
+# Edit M below to point to your local clone or MDD-DwC-mapping.
+
+M=~/g/MDD-DwC-mapping
+
+# MANUAL STEP: Get MDD 2.0.
+# Go to zenodo.org.  Search for "Mammal Diversity Database".  Select "Mammal
+# Diversity Database".  Select "Version v2.0".  Under "Files" select
+# "MDD_v2.0_6759species.csv".  Select "Download".  
+# Find MDD_v2.0_6759species.csv on your computer (e.g. in 'Downloads'). 
+# Move it to $L/in/.
+# Run conversion program MDD-DwC-mapping/ on it.
+
+# Prepare MDD 2.0 for alignment
 $M/src/explore_data.py --input $L/in/MDD_v2.0_6759species.csv \
   --output mdd2.0-raw.csv
 $P/clean.py --input mdd2.0-raw.csv >mdd2.0-clean.csv
